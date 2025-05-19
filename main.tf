b9097eb3d6e5e108cc8409689b381b6a5e71b478
Somnath Nelge <58774463+somnath1211@users.noreply.github.com>
2025-05-19 22:10:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,118 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+   region = "ap-south-1"
+  
+}
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-frontend-static-site-bucket"
+
+  tags = {
+    Name        = "StaticSiteBucket"
+    work = "assignment"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Bucket policy will be added after creating CloudFront OAC
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "StaticSiteOAC"
+  description                       = "OAC for S3 static site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Work = "assignment"
+  }
+}
+
+# Allow CloudFront OAC to access S3 bucket
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid: "DenyAccessToRestrictedFolders",
+        Effect: "Deny",
+        Principal: "*",
+        Action: "s3:GetObject",
+        Resource: [
+          "${aws_s3_bucket.static_site.arn}/public/private/*"
+        ]
+      },
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = ["s3:GetObject"]
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
